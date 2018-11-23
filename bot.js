abf1e816730caf323431d07f6c95c18a53201531
imraider <45240051+imraider@users.noreply.github.com>
2018-11-23 15:02:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ڙمۘــٰا̍نۨ ̨ڪــٰا̍ڣــﯧْۧــہ`,"http://twitch.tv/Death Shop")
+client.user.setGame(`ڪــٰا̍ڣــﯧْۧــہ ڙمۘــٰا̍نۨ `,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
