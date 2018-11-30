ce4c807e96552dfb2d1e3dd10607c8ca238b1de4
imraider <45240051+imraider@users.noreply.github.com>
2018-11-30 19:16:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,43 @@
+  client.on("message", function(message) {
+	  var prefix = "$"
+   if(message.content.startsWith(prefix + "rps")) {
+    let messageArgs = message.content.split(" ").slice(1).join(" ");
+    let messageRPS = message.content.split(" ").slice(2).join(" ");
+    let arrayRPS = ['**# - Rock**','**# - Paper**','**# - Scissors**'];
+    let result = `${arrayRPS[Math.floor(Math.random() * arrayRPS.length)]}`;
+    var RpsEmbed = new Discord.RichEmbed()
+    .setAuthor(message.author.username)
+    .setThumbnail(message.author.avatarURL)
+    .addField("Rock","??",true)
+    .addField("Paper","??",true)
+    .addField("Scissors","??",true)
+    message.channel.send(RpsEmbed).then(msg => {
+        msg.react(' ??')
+        msg.react("??")
+        msg.react("??")
+.then(() => msg.react('??'))
+.then(() =>msg.react('??'))
+.then(() => msg.react('??'))
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '??' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '??' && user.id === message.author.id;
+let reaction3Filter = (reaction, user) => reaction.emoji.name === '??' && user.id === message.author.id;
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+let reaction3 = msg.createReactionCollector(reaction3Filter, { time: 12000 });
+reaction1.on("collect", r => {
+        message.channel.send(result)
+})
+reaction2.on("collect", r => {
+        message.channel.send(result)
+})
+reaction3.on("collect", r => {
+        message.channel.send(result)
+})
+
+    })
+}
+});
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "+";
